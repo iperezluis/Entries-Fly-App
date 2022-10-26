d86ff379abafaa3a82975081542b9ec5c5475db6
iperezluis <perezvluisv@gmail.com>
2022-10-26 14:36:15 -0400

    dockerizando

--- database/db.ts
+++ database/db.ts
@@ -29,9 +29,9 @@ export const connect = async () => {
       await mongoose.disconnect();
     }
     // mongodb+srv://entries_db:Dinero8**@backend-cafe.y7fo5.mongodb.net/entriesdb
-    await mongoose.connect(process.env.DB_KEYS! || "");
+    await mongoose.connect(process.env.MONGO_URL! || "");
     mongoConnection.isConnected = 1;
-    console.log("conectado a mongoDB", process.env.DB_KEYS);
+    console.log("conectado a mongoDB", process.env.MONGO_URL);
   } catch (error) {
     console.log(error);
   }
