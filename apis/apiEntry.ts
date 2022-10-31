e5bbfcba26450d16abbb14eb73fa8daf86538b80
iperezluis <perezvluisv@gmail.com>
2022-10-31 10:36:00 -0400

    actualizando

--- apis/apiEntry.ts
+++ apis/apiEntry.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiEntry = axios.create({
-  baseURL: `${process.env.HOSTNAME}:8080/api`,
+  baseURL: `${process.env.HOSTNAME}:3000/api`,
 });
 
 export default apiEntry;
